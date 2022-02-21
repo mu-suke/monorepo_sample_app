00cf5eadb672f9bf9d477c3d779ec77a912c5139
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-02-21 15:54:24 +0900

    react-hook-formを試す用のloginページを作成

---
+++ src/components/forms/FormRow.tsx
@@ -0,0 +1,65 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react'
+import { css } from '@emotion/react'
+import React from 'react'
+import { FieldErrors } from 'react-hook-form'
+
+const FormRow: React.VFC<{
+  id: string
+  label?: string
+  isRequired?: boolean
+  helperText?: string
+  errors?: FieldErrors
+  labelStyle?: 'primary' | 'secondary'
+  children: React.ReactNode
+}> = ({
+        id,
+        label,
+        isRequired = false,
+        helperText,
+        errors,
+        labelStyle = 'primary',
+        children,
+      }) => {
+  const errorMessage = errors ? errors[id]?.message : undefined
+
+  return (
+    <>
+      <FormControl
+        id={id}
+        marginTop="1rem"
+        _first={{
+          marginTop: '0rem',
+        }}
+        isRequired={isRequired}
+        isInvalid={errorMessage}
+      >
+        {label && (
+          <FormLabel
+            htmlFor={id}
+            css={labelStyle === 'secondary' ? styleFormLabelSecondary : ''}
+          >
+            {label}
+          </FormLabel>
+        )}
+        {children}
+        {helperText && <FormHelperText>{helperText}</FormHelperText>}
+        {errorMessage && <FormErrorMessage>{errorMessage}</FormErrorMessage>}
+      </FormControl>
+    </>
+  )
+}
+
+const styleFormLabelSecondary = css({
+  fontWeight: 'normal',
+  marginTop: '1rem',
+  paddingLeft: '10px',
+  borderLeft: '5px solid',
+  borderColor: "orange",
+})
+
+export default FormRow
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from "axios";
 
 const Home: NextPage = () => {
   return (
@@ -51,6 +52,10 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
+        <button onClick={async () => {
+          const data = await axios.get('/api/hello')
+          console.log(data)
+        }}>API Route</button>
       </main>
 
       <footer className={styles.footer}>
