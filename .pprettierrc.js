8debd2eacdbd0f27b8af59a830c7adaeb59a43b5
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-03-04 14:56:52 +0900

    huskyとprettierの導入

---
+++ .pprettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  semi: false,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
