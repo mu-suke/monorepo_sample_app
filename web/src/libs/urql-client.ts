e0d39f58d4d75fb7c606470a6b7e918b76d12ef7
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-03-31 21:04:51 +0900

    未実装の箇所を削除

--- web/src/libs/urql-client.ts
+++ web/src/libs/urql-client.ts
@@ -1,42 +1,6 @@
-import { createClient, Exchange } from 'urql'
-import { empty, fromPromise, map, mergeMap, pipe } from 'wonka'
-import { auth } from '@/libs/firebase'
-
-const headerExchange: Exchange =
-  ({ forward }) =>
-  operations$ =>
-    pipe(
-      operations$,
-      mergeMap(operation => {
-        const currentUser = auth.currentUser
-        if (!currentUser) {
-          return pipe(
-            empty,
-            map(() => operation)
-          )
-        }
-        return pipe(
-          fromPromise(currentUser.getIdToken()),
-          map(authToken => {
-            console.log('authToken: ', authToken)
-            if (!operation.context.fetchOptions) {
-              operation.context.fetchOptions = {}
-            }
-            Object.assign(operation.context.fetchOptions, {
-              headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${authToken}`,
-              },
-            })
-            return operation
-          })
-        )
-      }),
-      forward
-    )
+import { createClient } from 'urql'
 
 export const urqlClient = createClient({
   url: 'http://localhost:8000/graphql',
   suspense: false,
-  exchanges: [headerExchange],
 })
