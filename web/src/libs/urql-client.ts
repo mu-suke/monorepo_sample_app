e24c9638c9668153625e747a6f40a4ca43a6117b
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-04-01 20:17:46 +0900

    authExchangeの導入

--- web/src/libs/urql-client.ts
+++ web/src/libs/urql-client.ts
@@ -1,6 +1,74 @@
-import { createClient } from 'urql'
+import { AuthConfig, authExchange } from '@urql/exchange-auth'
+import {
+  cacheExchange,
+  createClient,
+  dedupExchange,
+  fetchExchange,
+  makeOperation,
+} from 'urql'
+import { getAuthUser } from '@/libs/firebase'
+
+type AuthToken = {
+  token: string
+}
+
+const getAuth: AuthConfig<AuthToken>['getAuth'] = async () => {
+  try {
+    const user = await getAuthUser()
+    const token = await user.getIdToken()
+
+    if (!token) {
+      return null
+    }
+    return {
+      token,
+    }
+  } catch {
+    return null
+  }
+}
+
+const addAuthToOperation: AuthConfig<AuthToken>['addAuthToOperation'] = ({
+  authState,
+  operation,
+}) => {
+  if (!authState || !authState.token) {
+    return operation
+  }
+
+  const fetchOptions =
+    typeof operation.context.fetchOptions === 'function'
+      ? operation.context.fetchOptions()
+      : operation.context.fetchOptions || {}
+
+  return makeOperation(operation.kind, operation, {
+    ...operation.context,
+    fetchOptions: {
+      ...fetchOptions,
+      headers: {
+        ...fetchOptions.headers,
+        Authorization: `Bearer ${authState.token}`,
+      },
+    },
+  })
+}
+
+const didAuthError: AuthConfig<AuthToken>['didAuthError'] = ({ error }) => {
+  console.log('error: ', error)
+  return error.response.status === 401
+}
 
 export const urqlClient = createClient({
   url: 'http://localhost:8000/graphql',
   suspense: false,
+  exchanges: [
+    dedupExchange,
+    cacheExchange,
+    authExchange({
+      addAuthToOperation,
+      getAuth,
+      didAuthError,
+    }),
+    fetchExchange,
+  ],
 })
